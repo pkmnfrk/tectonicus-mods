6c247e16f8311a086d87b6e73be4b501ec43342e
Mike Caron <caron.mike@gmail.com>
2012-05-03 23:51:24 -0400

    actually load the IC2 config file

--- src/com/mikecaron/tectonicus/ic2/IC2Plugin.java
+++ src/com/mikecaron/tectonicus/ic2/IC2Plugin.java
@@ -1,9 +1,15 @@
 package com.mikecaron.tectonicus.ic2;
 
 import java.util.EnumSet;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.File;
+import java.io.IOException;
 import org.w3c.dom.Element;
 
 import tectonicus.Plugin;
+import tectonicus.BlockTypeRegistry;
 import tectonicus.configuration.XmlConfigurationParser;
 
 public class IC2Plugin implements Plugin
@@ -12,7 +18,13 @@ public class IC2Plugin implements Plugin
 		Plugin.Feature.BlockTypes
 	);
 	
-	private String configFilePath;
+	private File configFilePath;
+	private Properties ic2Config;
+	
+	public IC2Plugin()
+	{
+		ic2Config = new Properties();
+	}
 	
 	public String getName()
 	{
@@ -24,12 +36,35 @@ public class IC2Plugin implements Plugin
 		Element configFileNode = XmlConfigurationParser.getChild(config, "configFile");
 		
 		if(configFileNode != null) {
-			configFilePath = configFileNode.getNodeValue();
+			String p = XmlConfigurationParser.getString(configFileNode, "path");
+			
+			//System.out.println("IC2 config file: " + p);
+			
+			configFilePath = new File( p );
+			
+			try {
+				InputStream is = new FileInputStream( configFilePath );
+				
+				ic2Config.load(is);
+				
+				is.close();
+			} catch(IOException ex) {
+				// nom
+				throw new Error("Unable to read config file", ex);
+			}
+			
+			//System.out.println("IC2's machine block id is: " + ic2Config.getProperty("blockMachine", "null"));
+			
 		}
 	}
 	
 	public EnumSet<Plugin.Feature> getFeatures()
 	{
 		return features;
+	}
+	
+	public void registerBlockTypes(BlockTypeRegistry registry)
+	{
+		
 	}
 }
